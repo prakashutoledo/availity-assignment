e357e146996f72f564a9dd230e7a4a598ea38284
Prakash Khadka <prakashkhadka@aol.com>
2022-05-15 23:51:32 -0400

    Updated phone validation regex

--- registration-user-interface/src/components/registration-form.tsx
+++ registration-user-interface/src/components/registration-form.tsx
@@ -5,7 +5,7 @@ import axios from 'axios';
 
 const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 const zipRegex = /^\d{5}(?:[-\s]\d{4})?$/;
-const phoneRegex = /\+(9[976]\d|8[987530]\d|6[987]\d|5[90]\d|42\d|3[875]\d|2[98654321]\d|9[8543210]|8[6421]|6[6543210]|5[87654321]|4[987654310]|3[9643210]|2[70]|7|1)\d{1,14}$/;
+const phoneRegex = /[0-9]{10}/;
 const npiRegex = /[0-9]{10}/;
 
 export const apiGatewayEndpoint: any = process.env.REACT_APP_AWS_API_GATEWAY_ENDPOINT;
